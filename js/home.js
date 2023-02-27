d855f9db375487bd4ba3c2ba31cde6a61bd7bcc4
TeresitaFraszczak <110676528+TeresitaFraszczak@users.noreply.github.com>
2023-02-27 12:51:45 -0300

    iniciotask3

--- js/home.js
+++ js/home.js
@@ -1,27 +1,11 @@
-    /*htmlEvents += `<div class="col">
-                  <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                    <img src="${events.image}" class="card-img-top" alt="costume party">
-                    <div class="card-body">
-                      <h5 class="card-title text-center">${events.name}</h5>
-                      <p class="card-text">${events.description}</p>
-                      <div class="d-grid gap-2 d-md-flex justify-content-between">
-                        <p>Price:${events.price}</p>  
-                        <a href="#" class="btn btn-outline-danger ">ver más...</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>`;
+let htmlEvents = "";
+let cardContainer = document.getElementById("cardcontainer");
     
-    console.log(htmlEvents); */
-    
-    let htmlEvents = "";
-    let cardContainer = document.getElementById("cardcontainer");
-    
-    let currentDate = new Date(data.currentDate);
-    for (let event of data.events) {
-        let eventDate = new Date(event.date);
-        if (eventDate = currentDate) {
-            htmlEvents += createCard(event);
-        };
+  let currentDate = new Date(data.currentDate);
+   for (let event of data.events) {
+      let eventDate = new Date(event.date);
+      if (eventDate = currentDate) {
+          htmlEvents += createCard(event);
+       };
     };
-    cardContainer.innerHTML = htmlEvents; 
+  cardContainer.innerHTML = htmlEvents; 
--- js/upcoming.js
+++ js/upcoming.js
@@ -1,20 +1,3 @@
-
-
-    /*htmlEvents += `<div class="col">
-                  <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                    <img src="${events.image}" class="card-img-top" alt="costume party">
-                    <div class="card-body">
-                      <h5 class="card-title text-center">${events.name}</h5>
-                      <p class="card-text">${events.description}</p>
-                      <div class="d-grid gap-2 d-md-flex justify-content-between">
-                        <p>Price:${events.price}</p>  
-                        <a href="#" class="btn btn-outline-danger ">ver más...</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>`;
-    }
-    console.log(htmlEvents);  */  
 let htmlEvents = "";
 let cardContainer = document.getElementById("cardcontainer");
 
